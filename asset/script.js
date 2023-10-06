63059ba6284e03169c4f81c6b9e2340bd44d84a1
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-05 20:51:48 -0700

    Added comments

--- asset/script.js
+++ asset/script.js
@@ -1,42 +1,47 @@
-    // Get the current date and time using Day.js
-    var currentTime = dayjs().format("dddd DD");
-    
-    // Update the "timeHere" element with the current time
-    // document.getElementById("timeHere").textContent ="Today is: " + currentTime;
- $('#timeHere').text( "Today is: "+ currentTime);
-  // $('#timeDisplay').text(dayjs().format("hh:mm:ss"));
-  var timeArray= [$('#9AM'),$('#10AM'),$('#11AM'),$('#12PM'),$('#1PM'),$('#2PM'),$('#3PM'),$('#4PM') ];
-  
-  var n = 9;
 
-  function checkTime () {
-    console.log("Check time");
-    for (let i =0 ;i < timeArray.length;i++){
-        // timeArray[i]= n;
-        console.log('Element = ', timeArray[i]);
-        if ( n < dayjs().format("H")){
-            timeArray[i].addClass("past");
-            n++;
-    
-     
-          }
+var currentTime = dayjs().format("dddd DD");
 
+
+// I NEED TO PUT HOURS IN ORDER SO I WILL CHOSE AN ARRAY. I CAN USE IT INDEX TO DO SOME WORK.
+$("#timeHere").text("Today is: " + currentTime);
+var timeArray = [
+  $("#9AM"),
+  $("#10AM"),
+  $("#11AM"),
+  $("#12PM"),
+  $("#1PM"),
+  $("#2PM"),
+  $("#3PM"),
+  $("#4PM"),
+];
+
+// 9 IS HOUR STARTING HOUR HERE I SET THIS VARIABLE TO COMPARE IT WITH TIME
+var n = 9;
+function checkTime() {
+  console.log("Check time");
+  for (let i = 0; i < timeArray.length; i++) {
+    // timeArray[i]= n;
+    console.log("Element = ", timeArray[i]);
+    if (n < dayjs().format("H")) {
+      timeArray[i].addClass("past");
+      n++;
     }
   }
-  checkTime();
-
-  function updateClock() {
-    $('#timeDisplay').text(dayjs().format("hh:mm:ss"));
 }
-
-// Update the clock every second
+// AFTER THIS MANY MINUTES UPDATE THE CLOCK
+function updateClock() {
+  $("#timeDisplay").text(dayjs().format("hh:mm:ss"));
+}
 setInterval(updateClock, 1000);
 
-// Call updateClock function immediately to set the time immediately
-updateClock();
-  $('.saveBTN').on('click', function () {
-    var appt = $(this).siblings('.descr').val();
-    var time = $(this).parent().attr('id');
 
-    // save in localStorage
-    localStorage.setItem(time,appt);  })
+// THIS WILL SAVE THE INFORMATION TO JSON
+$(".saveBTN").on("click", function () {
+  var workOrder = $(this).siblings(".descr").val();
+  var timeOder = $(this).parent().attr("id");
+  var inputBox = $(this).siblings("input");
+  inputBox.addClass("booked");
+
+  // save in localStorage
+  localStorage.setItem(timeOrder, workOrder);
+});
